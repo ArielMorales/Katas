8b4b7369850e7669ed94585d1eaec41d97f9c024
ArialMorales <arial.morales@gmail.com>
2019-02-15 02:49:20 -0700

    Cesar: Implemented the logic

---
+++ src/Cesar/Cesar.java
@@ -0,0 +1,15 @@
+package Cesar;
+
+public class Cesar {
+	int clave;
+	final char[] ABC = new char [] {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	
+	public void setClave(int clave) {
+		this.clave = clave;
+	}
+	
+	public void codificar() {
+		
+	}
+
+}
